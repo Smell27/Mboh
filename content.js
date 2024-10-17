8c24c78ea299dbb5bdeb3b6bf7cc7b34fd4b6434
Smell27 <cokkgaming00@gmail.com>
2024-10-17 08:38:16 +0700

    Update content.js

--- content.js
+++ content.js
@@ -1,7 +1,4 @@
-// Mengubah teks di halaman web
 document.body.style.backgroundColor = "lightblue";
-
-// Menambahkan elemen baru
 let newElement = document.createElement("div");
 newElement.textContent = "Halo, ini adalah konten baru!";
 document.body.appendChild(newElement);
